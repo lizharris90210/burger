66aec272a069662d24725e90ccc223862a66764a
Liz Harris <lizharris90210@gmail.com>
2019-11-12 14:06:16 -0500

    sql and server.js setup

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL
+    AUTO_INCREMENT,
+  burger_name varchar
+    (200) NOT NULL,
+  devoured BOOLEAN default false,
+  primary key
+    (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,53 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Plain');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('with Cheese');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('with Bacon');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('with Cheese and Bacon');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('with Chili');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('with Cheese and Chili');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('with Cole Slaw');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('with Cole Slaw and Chili');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Vegetarian');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Fish');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Fish with Cole Slaw');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Turkey');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Turkey with Cheese');
+
